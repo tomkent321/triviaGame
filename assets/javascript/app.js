e7ae22dc7fd534b6b6e7721158f9e20d421de0a2
Thomas Hurd <thomashurd@Thomass-MacBook-Pro.local>
2018-08-09 13:08:12 -0600

    revisions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,17 +19,18 @@
 //Set up variables and arrays
 
 var arrQA= [    {q: "How many moons does Mars have?" , a: "Two", b: "Three", c: "Four", d: "It doesn't have any moons", r: "a" },
-                {q: "What speed does the 'C' in emc2 stand for?", a: "light", u: false},
-                {q: "What does the 'M' in 'emc2' stand for?", a: "Mass", u: false}
+                {q: "What speed does the 'C' in emc2 stand for?", a: "Two", b: "Three", c: "Four", d: "It doesn't have any moons", r: "c" },
+                {q: "What does the 'M' in 'emc2' stand for?", a: "Two", b: "Three", c: "Four", d: "It doesn't have any moons", r: "d" }
               
               ]; 
 
 var numQ;  //the number of this question (will not be the same question everytimd)
-var numQLeft; 
+var numQLeft = arrQA.length; //how many questions total 
 var ttlQ; // total number of question user can expect
 var qIndex = 0; //starts the game at the first question
 var qText; //used to find the question in the array not to show a number to the user 
 var userAnswer; 
+var answerGiven = false;
 var correct; 
 var sumCorrect;
 var pctCorrect;
@@ -51,9 +52,8 @@ function initialize(){
     pctCorrect = 0;
     timeLeft = 0;
     triviaIQ = 0;
-    
-    
-      
+    qIndex = 0;
+        
   }
  
 
@@ -72,15 +72,16 @@ function makeQuestion(){
 
 
 
-  $("#question").text(qText = arrQA[qIndex].q);
-  $(".inputLine").append("<p id= a>" + arrQA[qIndex].a + "</p>");
-  $(".inputLine").append("<p id= b>" + arrQA[qIndex].b + "</p>");
-  $(".inputLine").append("<p id= c>" + arrQA[qIndex].c + "</p>");
-  $(".inputLine").append("<p id= d>" + arrQA[qIndex].d + "</p>");
-  
-  alert("check");
+  $("#question").text(arrQA[qIndex].q);
+  $("#a").text(arrQA[qIndex].a);
+  $("#b").text(arrQA[qIndex].b);
+  $("#c").text(arrQA[qIndex].c);
+  $("#d").text(arrQA[qIndex].d);
+
+  qRun = true;
 
   timer();
+  //getAnswer();
 
   qIndex++;
 }
@@ -89,57 +90,119 @@ function makeQuestion(){
   
 
 
- //function answer()
+ function getAnswer() {
  
 
+// while (qRun == true&& answerGiven == false) {
 
-    //evalResponse
-              // capture response and store
+$("#a").on("click", function(){
+  userAnswer = "a";
+  
+  // scoreAnswer();
+});
 
-        //evaluate answer
-          //stop timer
-          //while time > 0
-            //if userAnswer == answer right score++
-              // correct++
-              //show correct panel
-              //update UI
-            //else if false 
-              //stop timer
-              //show incorrect notice  and correct answer below at the same time
-              //show correct answer
-              // set pause to see correct answer
-              // call show question
-              //update UI
+$("#b").on("click", function(){
+  userAnswer = "b";
+  
+  // scoreAnswer();
+});
+$("#c").on("click", function(){
+  userAnswer = "c";
+  
+  // scoreAnswer();
+});
+$("#d").on("click", function(){
+  userAnswer = "d";
+  // answerGiven = true;
+  // scoreAnswer();
+});
 
-    // else if no answer 
-          // show time out response
-          //show incorrect notice
-          //show correct answer
-          // set pause to see correct answer
-          // call show question
-          //update UI
+// }
+clearInterval(XPathExpression);
+ }
+
+ function scoreAnswer() {
+
+  if ( userAnswer === arrQA[qIndex].r) {
+  // right answer
+  $(".feedback").text("Correct!").attr("color", "green");
+  sumCorrect++;
+
+
+  } else {
+    //wrong answer
+    $(".feedback").text("Sorry!").attr("color", "red");
+
+  }
+
+  showRightAnswer();
+numQLeft--;
+
+} 
+
+function showRightAnswer() {
+
+
+
+  if(arrQA[qIndex].r == "a"){
+    $("#a").text(arrQA[qIndex].a).attr("fontWeight","bolder");
+  }else{
+    $("#a").text("                               ");
+  }
+
+  if(arrQA[qIndex].r == "b"){
+    $("#b").text(arrQA[qIndex].b).attr("fontWeight","bolder");
+  }else{
+  $("#b").text("                               ");
+  }
+
+  if(arrQA[qIndex].r == "c"){
+    $("#c").text(arrQA[qIndex].b).attr("fontWeight","bolder");
+  }else{
+  $("#c").text("                               ");
+  }
+
+  if(arrQA[qIndex].r == "d"){
+    $("#d").text(arrQA[qIndex].b).attr("fontWeight","bolder");
+  }else{
+  $("#d").text("                               ");
+  }
+
+  }
 
 
 
 
-    //update UI
-      //show scores
-          
-          //remove win or loss notice
-          //# of questions given
-          //# questions remaining
-          // questions right
-          // questions wrong
-          // Trivia IQ
-          //stop timer just to be sure
+function updateAndNextQuestion() {
 
-    //
-            
-    //shorPause
-              //giving time to see right answer
-              
+timerShort();
 
-    //initialize/restart
+
+if(numQLeft > 0) {
+
+  $("#a").text("");
+  $("#b").text("");
+  $("#c").text("");
+  $("#d").text("");
+
+  
+} else {
+
+  gameOver();
+}
+
+  }
+
+  
+
+function gameOver() {
+
+
+
+}
+
+
+
 
 
             
@@ -155,18 +218,44 @@ function makeQuestion(){
       var timeLeft = targetTime - timeNow;
       var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
         
+      if(seconds > 0){
         $("#time-left").html("<h2>" + seconds + "</h2>");
-      
+      }
         // If the count down is finished, write some text 
-        if (timeLeft < 0) {
-          clearInterval(XPathExpression);
+        if (timeLeft < 1) {
+          clearInterval(x);
           //question time is up
-
-          $("#time-left").html("<h2>Times UP!</h2>");
+          qRun = false;
+          //$("#time-left").html("<h2>Times UP!</h2>");
+          $(".feedback").text("Sorry!").attr("color", "red");
+          //showRightAnswer();
         }
       }, 1000);
       
       }
+
+      function timerShort() {
+      
+        targetTime = new Date().getTime() + 2000;
+  
+        // Update the count down every 1 second
+        var x = setInterval(function() {
+        var timeNow =  new Date().getTime(); 
+        var timeLeft = targetTime - timeNow;
+        var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+          
+          //$("#time-left").html("<h2>" + seconds + "</h2>");
+        
+          // If the count down is finished, write some text 
+          if (timeLeft < 0) {
+            clearInterval(XPathExpression);
+            //question time is up
+            //qRun = false;
+            //$("#time-left").html("<h2>Times UP!</h2>");
+          }
+        }, 1000);
+        
+        }
 
 
 
